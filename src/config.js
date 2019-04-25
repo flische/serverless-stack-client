cfece61018e99e37ae3459125505194a06d2ea4d
flische <flische@gmail.com>
2019-04-25 13:57:30 -0700

    updated package.json

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-west-2",
+    BUCKET: "federicos-notes-app-uploads"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-west-2",
+    URL: "https://db3g9zex6c.execute-api.us-west-2.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_vsjrcDRXK",
+    APP_CLIENT_ID: "6e2e4uggg8fjsk7lqctklm9opi",
+    IDENTITY_POOL_ID: "us-west-2:9d3b6265-48a5-4d3e-badd-b3b70b87c710"
   }
 };
