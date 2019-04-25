248029ff35bb91b5dcda05f5b965044d5d66f855
flische <flische@gmail.com>
2019-04-25 14:16:29 -0700

    Adding Stripe public key to config file

--- src/config.js
+++ src/config.js
@@ -13,5 +13,6 @@ export default {
     USER_POOL_ID: "us-west-2_vsjrcDRXK",
     APP_CLIENT_ID: "6e2e4uggg8fjsk7lqctklm9opi",
     IDENTITY_POOL_ID: "us-west-2:9d3b6265-48a5-4d3e-badd-b3b70b87c710"
-  }
+  },
+  STRIPE_KEY: "pk_test_MjAxVq7zCOnLKBEP5AXUs4VY00tPvWvjLT",
 };
